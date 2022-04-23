44f642aebeaebd090fa3e1b5b8d9dafe1e468b79
ldcu <98633536+ykuw@users.noreply.github.com>
2022-04-23 22:27:35 +0300

    312b73a9ae39159f373d53138dd11cf4

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,9 +4,9 @@ import {Helmet} from "react-helmet";
 
 export default class Home extends React.Component {
 	state = {
-		quotes: [],
-		isLoading: true,
-		errors: null,
+		quotes: [], // Array of quote objects.
+		isLoading: true, // Loading state.
+		errors: null, // Error state.
 	};
 
 	getQuotes() {
@@ -19,67 +19,69 @@ export default class Home extends React.Component {
 		})
 			.then((response) => {
 				this.setState({
-					quotes: response.data,
-					isLoading: false,
+					quotes: response.data, // Data is an array of quotes.
+					isLoading: false, // Set loading to false.
 				});
 			})
 			.catch((error) => this.setState({error, isLoading: false}));
 	}
 
-	componentDidMount() {
-		this.getQuotes();
+	componentDidMount() { // When the component mounts, get the quotes.
+		this.getQuotes(); // Get quotes.
+		this.interval = setInterval(() => this.setState({ time: Date.now() }), 1); // Set interval for time to refresh every millisecond.
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.interval); // Clear interval when component is unmounted. So that it doesn't keep running.
 	}
 
 	render() {
-		const day = 21, month = "Apr", year = 1996;
 
-		let age = calculateAge(`${month} ${day} ${year}`);
+		const {isLoading, quotes} = this.state; // Destructure state.
 
-		const {isLoading, quotes} = this.state;
-
-		return (
+		return ( // Render.
 			<>
-				<div className="container">
-					<Helmet>
-						<title>Home</title>
+				<div className="container"> {/* Container for the page. */}
+					<Helmet> {/* Helmet for the page. */}
+						<title>Home</title> {/* Title for the page. */}
 					</Helmet>
 
-					<h1>Quote of the day</h1>
-					<br/>
+					<h1>Quote</h1>
+					<br/> {/* Break. */}
 
-					<div className="tab-space">
-						{!isLoading ? (
-							quotes.map((fields) => {
+					<div className="tab-space"> {/* Container for the tabs. */}
+						{!isLoading ? ( // If not loading, render.
+							quotes.map((fields) => { // Map through quotes.
 								const {_id, quote, author, source} = fields; // Getting the fields in a const as it is neater and more informative.
-								return (
-									<div key={_id}>
-										<blockquote>
+								return ( // Render.
+									<div key={_id}> {/* Container for the quote. */}
+										<blockquote> {/* Quote. */}
 											<p>"{quote}"</p>
-											<footer>
+											<footer> {/* Footer for the quote. */}
 												{author} in <cite title="Source">{source}</cite>
 											</footer>
 										</blockquote>
 									</div>
-								);
-							})
-						) : (
-							<p>Loading...</p>
-						)}
-					</div>
+								); // End render.
+							}) // End map.
+						) : ( // Else, render.
+							<p>Loading...</p> // Loading.
+						)} {/* End render. */}
+					</div> {/* End container for the tabs. */}
 
-					<br/>
+					<br/> {/* Break. */}
 					<h1>Age</h1>
 					<br/>
 
 					<div className="tab-space">
-						<p>{age} old.</p>
+						<p>{displayAge()}</p> {/* Display age. */}
 					</div>
 
 					<br/>
 
-					<div align="left" className="fixed-bottom">
+					<div align="left" className="fixed-bottom"> {/* Container for the footer. */}
 						<p/>
-						<p className="tab-space">&nbsp;&nbsp;'sup</p>
+						<p className="tab-space">&nbsp;&nbsp;'sup</p> {/* Footer for the page. */}
 						<p/>
 					</div>
 				</div>
@@ -88,9 +90,16 @@ export default class Home extends React.Component {
 	}
 }
 
-// Function to calculate the age of a person.
-function calculateAge(birthday) {
-	const ageDifMs = Date.now() - new Date(birthday).getTime();
-	const ageDate = new Date(ageDifMs); // Milliseconds from epoch.
-	return Math.abs(ageDate.getUTCFullYear() - 1970);
+// Function to calculate the age of a person, down to year, month, day, hour, minute, second and millisecond.
+function displayAge() {
+	const currentDate = new Date(); // Get current date.
+	const birthDate = new Date("1996-04-21T00:00:00"); // Get birthdate.
+	const y = currentDate.getFullYear() - birthDate.getFullYear(); // Get the year.
+	const m = currentDate.getMonth() - birthDate.getMonth(); // Get the month.
+	const h = currentDate.getHours() - birthDate.getHours(); // Get the hour.
+	const d = currentDate.getDate() - birthDate.getDate(); // Get the day.
+	const min = currentDate.getMinutes() - birthDate.getMinutes(); // Get the minute.
+	const s = currentDate.getSeconds() - birthDate.getSeconds(); // Get the seconds.
+	const ms = currentDate.getMilliseconds() - birthDate.getMilliseconds(); // Get the milliseconds.
+	return y + " years, " + m + " months, " + d + " days, " + h + " hours, " + min + " minutes, " + s + " seconds and "+ ms + " milliseconds old.";
 }
