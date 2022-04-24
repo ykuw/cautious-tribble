93c180b61bc2b1ee1fddd146ae2d97ed857c3a8b
ldcu <98633536+ykuw@users.noreply.github.com>
2022-04-24 16:27:05 +0300

    312b73a9ae39159f373d53138dd11cf4

--- index.js
+++ index.js
@@ -10,21 +10,25 @@ require("dotenv").config();
 
 // Import models.
 
-require('./models/posts');
-require('./models/quotes');
-require('./models/users');
+require('./collections/posts');
+require('./collections/quotes');
+require('./collections/users');
 
 // Initiate MongoDB connection.
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(r => {
+    console.log('Connected to MongoDB.');
+}).catch(e => {
+    console.log('Error connecting to MongoDB.');
+});
 
 app.use(bodyParser.json());
 
 // Import routes.
 
-require('./routes/posts')(app);
-require('./routes/quotes')(app);
-require('./routes/users')(app);
+require('./apis/posts')(app);
+require('./apis/quotes')(app);
+require('./apis/users')(app);
 
 // Choose port & start the server.
 
@@ -37,10 +41,10 @@ app.listen(PORT, () => {
 
 // 1. Serve static files from the React frontend app
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'interface/build')))
 
 // 2. Anything that doesn't match the above, send back index.html
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    res.sendFile(path.join(__dirname + '/interface/build/index.html'))
 })
--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,8 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => { // If the API executed doesn't have a valid "access_token", then it won't work to be executed.
     try {
         const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
-        req.userData = decoded;
+        req.userData = jwt.verify(token, process.env.JWT_KEY);
         next();
     } catch (error) {
         return res.status(401).json({
