328febb0efeed01c11996d816d579e0b9c25d63c
ykuw <lentils-aspic0p@icloud.com>
2022-05-17 14:38:26 +0300

    312b73a9ae39159f373d53138dd11cf4

--- server/index.js
+++ server/index.js
@@ -5,17 +5,14 @@ const app = express();
 const path = require('path');
 
 // Read .env files.
-
 require("dotenv").config();
 
 // Import models.
-
 require('./collections/posts');
 require('./collections/quotes');
 require('./collections/users');
 
 // Initiate MongoDB connection.
-
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(r => {
     console.log('Connected to MongoDB.');
 }).catch(e => {
@@ -25,13 +22,11 @@ mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopo
 app.use(bodyParser.json());
 
 // Import routes.
-
 require('./apis/posts')(app);
 require('./apis/quotes')(app);
 require('./apis/users')(app);
 
 // Choose port & start the server.
-
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Mixing it up on port ${PORT}.`)
@@ -40,11 +35,9 @@ app.listen(PORT, () => {
 // This is to serve up the React files once it's in production. (1, 2).
 
 // 1. Serve static files from the React front-end app.
-
 app.use(express.static(path.join(__dirname, '../interface/build')))
 
 // 2. Anything that doesn't match the above, send back index.html.
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '../interface/build/index.html'))
 })
