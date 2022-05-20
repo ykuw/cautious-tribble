16bb53b3060e89cc1323bd07055a8ca14119fc5d
ykuw <98633536+ykuw@users.noreply.github.com>
2022-05-20 14:01:05 +0300

    312b73a9ae39159f373d53138dd11cf4

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-const path = require('path');
 
 // Read .env files.
 require("dotenv").config();
