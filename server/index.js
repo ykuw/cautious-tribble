d7c46054f6d448a2dfb73221f4573f19eea18327
ykuw <98633536+ykuw@users.noreply.github.com>
2022-05-20 14:03:03 +0300

    312b73a9ae39159f373d53138dd11cf4

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 // Read .env files.
 require("dotenv").config();
@@ -31,3 +32,12 @@ app.listen(PORT, () => {
     console.log(`Mixing it up on port ${PORT}.`)
 })
 
+// This is to serve up the React files once it's in production. (1, 2).
+
+// 1. Serve static files from the React front-end app.
+app.use(express.static(path.join(__dirname, '../interface/build')))
+
+// 2. Anything that doesn't match the above, send back index.html.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '../interface/build/index.html'))
+})
